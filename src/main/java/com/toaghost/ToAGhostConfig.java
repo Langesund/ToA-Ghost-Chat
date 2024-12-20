d70f3ec90a31a944512288ba204965225d9f6f79
Langesund <55497030+Langesund@users.noreply.github.com>
2024-12-20 23:38:13 +0100

    Update ToAGhostConfig.java

--- src/main/java/com/toaghost/ToAGhostConfig.java
+++ src/main/java/com/toaghost/ToAGhostConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("toaghostchat")
 public interface ToAGhostConfig extends Config
 {
 	@ConfigItem(
